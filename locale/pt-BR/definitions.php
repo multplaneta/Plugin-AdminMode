fa864215d17fbcf9fad99767ff33c22c6001a53f
multplaneta <THEODEFRANCA@gmail.com>
2014-09-24 18:04:52 -0300

    Revert "Translation to pr-BR"
    This reverts commit aa0e88efa04779ab53f72b095653d2b6c139fb44.

--- locale/pt-BR/definitions.php
+++ locale/pt-BR/definitions.php
@@ -1,4 +1,4 @@
 <?php if (!defined('APPLICATION')) exit();
 /* Copyright 2014 Zachary Doll */
-$Definition['Plugins.AdminMode.DisableLink'] = '<span class="InformSprite Lightbulb"></span> Desativar o modo de manutenção';
+$Definition['Plugins.AdminMode.DisableLink'] = '<span class="InformSprite Lightbulb"></span> Desativar o modo de manutencão';
 $Definition['Plugins.AdminMode.EntryLink'] = '<span class="InformSprite Key"></span> Conectar';
