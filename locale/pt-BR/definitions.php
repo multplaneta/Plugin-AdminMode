c895e2f5f65663bb1fe33b0ef11a3b78d29510b6
multplaneta <THEODEFRANCA@gmail.com>
2014-09-24 18:01:25 -0300

    Revert "Translation to pt-BR"
    This reverts commit c9d73d603dfe2509633c3c2d05712dd48232795f.

--- locale/pt-BR/definitions.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-/* Copyright 2014 Zachary Doll */
-$Definition['Plugins.AdminMode.DisableLink'] = '<span class="InformSprite Lightbulb"></span> Desativar o modo de manutenção';
-$Definition['Plugins.AdminMode.EntryLink'] = '<span class="InformSprite Key"></span> Conectar';
